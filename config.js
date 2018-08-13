9616e0af3ec9d40dce66cf88b742272c03edd712
조윤범 <jyb01124@naver.com>
2018-08-13 20:19:40 +0900

    MQTT_pub&sub_OK
    코드 수정해서 publish가능하게 완료

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // host = '172.16.153.122';	// hostname or IP address
-host = '127.0.0.1';	// hostname or IP address
+host = '192.168.123.1';	// hostname or IP address
 // host = '172.16.153.110';	// hostname or IP address
-port = 9001;
+port = 8080;
 topic = '#';		// topic to subscribe to
 useTLS = false;
 username = null;
